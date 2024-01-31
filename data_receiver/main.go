5d945c8123d91ec3e4eb5b9ab9dc6450bbc2abe7
Marc <ninjaz.gg@outlook.com>
2024-01-30 19:26:30 -0500

    push obu data to kafka

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/websocket"
 	"github.com/umizu/tollcalc/types"
 )
 
+const kafkaTopic = "obudata"
+
 func main() {
-	receiver := NewDataReceiver()
+	receiver, err := NewDataReceiver()
+	if err != nil {
+		panic(err)
+	}
+
+	defer receiver.prod.Close()
 	http.HandleFunc("/ws", receiver.handleWS)
 	http.ListenAndServe(":30000", nil)
 }
@@ -18,12 +27,50 @@ func main() {
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
+func (dr *DataReceiver) produceObuData(data types.OBUData) error {
+	// Produce messages to topic (asynchronously)
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	topic := kafkaTopic
+	if err = dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny},
+		Value: b,
+	}, nil); err != nil {
+		return err
+	}
+
+	go func() {
+		for e := range dr.prod.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return nil
+}
+
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+
 	return &DataReceiver{
 		msgch: make(chan types.OBUData, 128),
-	}
+		prod:  p,
+	}, nil
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +94,9 @@ func (dr *DataReceiver) wsReceiveLoop() {
 		if err := dr.conn.ReadJSON(&data); err != nil {
 			log.Println("read error:", err)
 		}
-		fmt.Printf("received OBU data from [%d]: <lat %.2f, lng %.2f>\n", data.OBUId, data.Lat, data.Lon)
-		dr.msgch <- data
+		// fmt.Printf("received OBU data from [%d]: <lat %.2f, lng %.2f>\n", data.OBUId, data.Lat, data.Lon)
+		if err := dr.produceObuData(data); err != nil {
+			log.Println("produce error:", err)
+		}
 	}
 }
